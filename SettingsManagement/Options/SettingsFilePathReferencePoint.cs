a97a1bf518d363fd154839e8fc32177a6f9668e5
KPKirilov <k.p.kirilov@abv.bg>
2021-05-18 01:41:48 +0300

    #21 Removed the reference assembly property for the settings file path.

--- SettingsManagement/Options/SettingsFilePathReferencePoint.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SettingsManagement.Options
-{
-    public enum SettingsFilePathReferencePoint
-    {
-        CallingAssembly = 0,
-        ExecutingAssembly = 1,
-        Custom = 2,
-    }
-}
